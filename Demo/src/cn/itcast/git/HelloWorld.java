e47d82eec31ca5f2d9d21cfc9c92de76b87ebc4f
tyh <690215996@qq.com>
2018-02-28 21:50:15 +0800

    同分支冲突测试副本修改

--- Demo/src/cn/itcast/git/HelloWorld.java
+++ Demo/src/cn/itcast/git/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("helloworld!");
 		System.out.println("helloworld!");
-		System.out.println("helloworld!");
+		System.out.println("hhh");
 	}
 	
 }
